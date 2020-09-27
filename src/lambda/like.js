2ffb6378e690f718bd1ec5c155ce4ee102635dc1
Cleiton Nunes <cleiton.rodrigues6102@gmail.com>
2020-09-27 13:59:38 -0300

    refactor: now like posts by id

--- src/lambda/like.js
+++ src/lambda/like.js
@@ -1,50 +1,63 @@
 import faunadb from 'faunadb';
 
+import getId from './utils/getId';
+
 const query = faunadb.query;
 const client = new faunadb.Client({
   secret: process.env.FAUNADB_SECRET
 })
 
-exports.handler = (event, context, callback) => {
-  client.query(
-    query.Update(
-      query.Select("ref", query.Get(
-        query.Match(
-          query.Index("post_by_id"), 
-          1
-        )
-      )),
-      {
-        data: {
-          likes: query.Add(
-            query.Select(
-              ['data', 'likes'],
-              query.Get(
-                query.Select(
-                  "ref",
-                  query.Get(
-                    query.Match(
-                      query.Index("post_by_id"), 1
+exports.handler = async (event, context) => {
+  const id = getId(event.path)
+
+  if(!id) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({message: 'You need to send the post id in url'})
+    }
+  }
+
+  try {
+    const {data} = await client.query(
+      query.Update(
+        query.Select("ref", query.Get(
+          query.Match(
+            query.Index("post_by_id"), 
+            Number(id)
+          )
+        )),
+        {
+          data: {
+            likes: query.Add(
+              query.Select(
+                ['data', 'likes'],
+                query.Get(
+                  query.Select(
+                    "ref",
+                    query.Get(
+                      query.Match(
+                        query.Index("post_by_id"), 
+                        Number(id)
+                      )
                     )
                   )
                 )
-              )
-            ),
-            1
-          )
+              ),
+              1
+            )
+          }
         }
-      }
+      )
     )
-  ).then((response) => {
-    callback(null, {
+  
+    return {
       statusCode: 200,
-      body: JSON.stringify({likes: response.data.likes})
-    })
-  }).catch((e) => {
-    console.log("Erro", e)
-    callback(null, {
+      body: JSON.stringify({likes: data.likes})
+    }
+  } catch (error) {
+    return {
       statusCode: 400,
-      body: JSON.stringify({message: 'Erro interno'})
-    })
-  })
+      body: JSON.stringify({message: 'Internal error'})
+    }
+  }
 }
