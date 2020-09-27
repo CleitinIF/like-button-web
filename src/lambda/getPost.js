60602e018a62b950d99efee79e73028117df5142
Cleiton Nunes <cleiton.rodrigues6102@gmail.com>
2020-09-27 13:59:51 -0300

    refactor: now get post by id

--- src/lambda/getPost.js
+++ src/lambda/getPost.js
@@ -1,30 +1,40 @@
 import faunadb from 'faunadb';
 
+import getId from './utils/getId';
+
 const query = faunadb.query;
 const client = new faunadb.Client({
   secret: process.env.FAUNADB_SECRET
 })
 
-exports.handler = (event, context, callback) => {
-  // console.log(event);
-  client.query(
-    query.Select("data", query.Get(
-      query.Match(
-        query.Index("post_by_id"), 
-        1
-      )
-    ))
-  ).then((data) => {
-    console.log(JSON.stringify(data))
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(data)
-    })
-  }).catch((e) => {
-    console.log("Erro", e)
-    callback(null, {
+exports.handler = async (event, context) => {
+  const id = getId(event.path)
+
+  if(!id) {
+    return {
       statusCode: 400,
-      body: JSON.stringify({message: 'Erro interno'})
-    })
-  })
+      body: JSON.stringify({message: 'You need to send the post id in url'})
+    }
+  }
+
+  try {
+    const response = await client.query(
+      query.Select("data", query.Get(
+        query.Match(
+          query.Index("post_by_id"), 
+          id
+        )
+      ))
+    )
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    }
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({message: 'Internal error'})
+    }
+  }
 }
