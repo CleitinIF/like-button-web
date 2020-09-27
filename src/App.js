4ab7c1818fde5ab605890b771b0122f464473f82
Cleiton Nunes <cleiton.rodrigues6102@gmail.com>
2020-09-27 14:00:19 -0300

    feat: like button by id and clap animation

--- src/App.js
+++ src/App.js
@@ -1,33 +1,75 @@
-import React, { Component, useEffect, useState } from "react"
-import logo from "./logo.svg"
+import React, { useEffect, useRef, useState } from "react"
 import "./App.css"
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [likesSent, setLikesSent] = useState(false);
   const [post, setPost] = useState();
 
+  const imageRef = useRef(null);
+
   const fetchPost = async () => {
-    fetch("/.netlify/functions/getPost")
+    setLoading(true)
+
+    fetch("/.netlify/functions/getPost/1")
       .then(response => response.json())
-      .then(json => setPost(json))
+      .then(json => {
+        setPost(json)
+        setLoading(false)
+      })
   }
 
   useEffect(() => {
     fetchPost();
   }, [])
 
-  const handleLikeClick = (state) => {
-    fetch("/.netlify/functions/" + state)
-      .then(response => response.json())
-      .then(json => setPost(prev => ({
-        ...prev,
-        likes: json.likes
-      })))
+  const handleClapAnimation = () => {
+    imageRef.current.classList.add('clapping')
+    imageRef.current.timer = setTimeout(() => {
+      imageRef.current.classList.remove('clapping')
+    }, 200)
+  }
+
+  const handleLikeClick = () => {
+    if(likesSent >= 50) {
+      window.alert('You can only send 50 likes per post!')
+      return
+    }
+
+    handleClapAnimation();
+    setLikesSent((prev) => prev+1);
+    setPost(prev => ({
+      ...prev,
+      likes: prev.likes + 1
+    }))
+    fetch("/.netlify/functions/like/1")
+      .catch(() => {
+        setLikesSent(prev => prev-1);
+        setPost(prev => ({
+          ...prev,
+          likes: prev.likes - 1
+        }))
+      })
   }
 
   return (
-    <div className="App">
-      <p>Artigo: {post?.likes} likes</p>
-      <button onClick={() => handleLikeClick('like')}>Like</button>
+    <div className="container">
+      {loading ? (
+        <div className="loading"/>
+      ) : (
+        <>
+          <div className="content">
+            <p>Post {post.name}</p>
+            <button onClick={() => handleLikeClick('like')}>
+              <img ref={imageRef} src={require(`./assets/${likesSent ? 'filled-clap.png' : 'clap.png'}`)}/>
+            </button>
+            <p>{post?.likes} likes</p>
+          </div>
+          <div className="footer">
+            Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+          </div>
+        </>
+      )}
     </div>
   )
 }
