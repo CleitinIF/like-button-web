e7b338e8256ae8b7c497f942d6340dd61b7cfb99
Cleiton Nunes <cleiton.rodrigues6102@gmail.com>
2020-09-28 16:27:03 -0300

    refactor: remove netlify dependencies and functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef, useState } from "react"
-import "./App.css"
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -9,72 +9,91 @@ const App = () => {
   const imageRef = useRef(null);
 
   const fetchPost = async () => {
-    setLoading(true)
+    setLoading(true);
 
-    fetch("/.netlify/functions/getPost/1")
-      .then(response => response.json())
-      .then(json => {
-        setPost(json)
-        setLoading(false)
-      }).catch(() => {
-        window.alert('An error occurred while making the request')
+    fetch(`${process.env.REACT_APP_API_URL}/getpost/1`, {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        setPost(json);
+        setLoading(false);
       })
-  }
+      .catch(() => {
+        window.alert("An error occurred while making the request");
+      });
+  };
 
   useEffect(() => {
     fetchPost();
-  }, [])
+  }, []);
 
   const handleClapAnimation = () => {
-    imageRef.current.classList.add('clapping')
+    imageRef.current.classList.add("clapping");
     imageRef.current.timer = setTimeout(() => {
-      imageRef.current.classList.remove('clapping')
-    }, 200)
-  }
+      imageRef.current.classList.remove("clapping");
+    }, 200);
+  };
 
   const handleLikeClick = () => {
-    if(likesSent >= 50) {
-      window.alert('You can only send 50 likes per post!')
-      return
+    if (likesSent >= 50) {
+      window.alert("You can only send 50 likes per post!");
+      return;
     }
 
     handleClapAnimation();
-    setLikesSent((prev) => prev+1);
-    setPost(prev => ({
+    setLikesSent((prev) => prev + 1);
+    setPost((prev) => ({
       ...prev,
-      likes: prev.likes + 1
-    }))
-    fetch("/.netlify/functions/like/1")
-      .catch(() => {
-        window.alert('An error occurred while making the request')
-        setLikesSent(prev => prev-1);
-        setPost(prev => ({
-          ...prev,
-          likes: prev.likes - 1
-        }))
-      })
-  }
+      likes: prev.likes + 1,
+    }));
+    fetch(`${process.env.REACT_APP_API_URL}/like/1`).catch(() => {
+      setLikesSent((prev) => prev - 1);
+      setPost((prev) => ({
+        ...prev,
+        likes: prev.likes - 1,
+      }));
+      window.alert("An error occurred while making the request");
+    });
+  };
 
   return (
     <div className="container">
       {loading ? (
-        <div className="loading"/>
+        <div className="loading" />
       ) : (
         <>
           <div className="content">
             <p>{post.name}</p>
-            <button onClick={() => handleLikeClick('like')}>
-              <img ref={imageRef} src={require(`./assets/${likesSent ? 'filled-clap.png' : 'clap.png'}`)} alt="Clap button" />
+            <button onClick={() => handleLikeClick("like")}>
+              <img
+                ref={imageRef}
+                src={require(`./assets/${
+                  likesSent ? "filled-clap.png" : "clap.png"
+                }`)}
+                alt="Clap button"
+              />
             </button>
             <p>{post?.likes} likes</p>
           </div>
           <div className="footer">
-            Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+            Icons made by{" "}
+            <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+              Freepik
+            </a>{" "}
+            from{" "}
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              {" "}
+              www.flaticon.com
+            </a>
           </div>
         </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/lambda/utils/getId.js
+++
@@ -1,3 +0,0 @@
-module.exports = function getId(urlPath) {
-  return Number(urlPath.match(/([^\/]*)\/*$/)[0])
-}
